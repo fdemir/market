30dbabed3a44576b552e5886a81413cd13d8f5d4
fdemir <frndemir@gmail.com>
2022-10-02 21:22:31 +0300

    integrate cypress component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
