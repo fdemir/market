f86c3e8e9daf48aaec16f52096c23215cd166d77
fdemir <frndemir@gmail.com>
2022-10-02 09:13:50 +0300

    fix logo position at small screens

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -28,6 +28,11 @@ const StyledLogo = styled(Logo)`
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
+
+  @media (max-width: ${(props) => props.theme.breakpoints.md}) {
+    left: 16px;
+    transform: translateX(0);
+  }
 `;
 
 const StyledHeaderContainer = styled(Container)`
