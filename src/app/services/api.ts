43457dfdeb848ef269a4ad98d893fed6553c048c
fdemir <frndemir@gmail.com>
2022-10-02 07:17:08 +0300

    add production url

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { Product } from "~/types";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://plankton-app-5vszu.ondigitalocean.app/api",
 });
 
 export const api = createApi({
